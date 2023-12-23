6a275f2bd1daf08c909eb903f632a24f4285f933
darishkhan <darishkhanon155@gmail.com>
2023-12-23 15:49:57 +0530

    hosting

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose=require('mongoose')
-const dotenv=require('dotenv')
-const cors=require('cors')
-const path=require("path")
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-dotenv.config()
+dotenv.config();
 
 // Database connection
-const connectDB=async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("database is connected successfully!")
-
-    }
-    catch(err){
-        console.log(err)
-    }
-}
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("database is connected successfully!");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 // Middlewares
-app.use(express.json())
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//deployment
+const _dirname = path.dirname("");
+const buildpath = path.join(_dirname, "../frontend/dist");
+app.use(express.static(buildpath));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../frontend/dist/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 // Route imports
 const user = require("./routes/userRoute");
-const invoice = require('./routes/invoice');
+const invoice = require("./routes/invoice");
 
-app.use("/api/v1",user);
+app.use("/api/v1", user);
 app.use("/api/invoice", invoice);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    connectDB()
-    console.log("app is running on port "+PORT)
-}) 
+app.listen(PORT, () => {
+  connectDB();
+  console.log("app is running on port " + PORT);
+});
